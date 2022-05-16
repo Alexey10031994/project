b68aefefed41872a790e7b4777fd4dd4e2f7e00d
ALEXEY <alexseva94@gmail.com>
2022-05-16 16:42:26 +0300

    PRACTICUM#1

--- JS LESSONS/js/script.js
+++ JS LESSONS/js/script.js
@@ -1,2 +1,21 @@
 "use strict";
 
+const numberOfFilms = +prompt("сколько фильмов вы уже смотрели?", "");
+
+const personMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt("Один из полседних фильмов?", ""),
+    b = prompt("Оценка фильма?", ""),
+    c = prompt("Один из полседних фильмов?", ""),
+    d = prompt("Оценка фильма?", "");
+
+personMovieDB.movies[a] = b;
+personMovieDB.movies[c] = d;
+
+console.log(personMovieDB);
